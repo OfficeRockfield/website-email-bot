5de7daae5d260015f6319c35df170f2e778425f4
Eoin Gaughran <eoinbgaughran@gmail.com>
2023-06-16 17:52:03 +0200

    sendinblue-dotenv

--- sendEmail.js
+++ sendEmail.js
@@ -1,26 +1,24 @@
 "use strict";
 const nodemailer = require("nodemailer");
+require('dotenv').config();
 
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "smtp-relay.sendinblue.com",
     port: 587,
     secure: false, // true for 465, false for other ports
     auth: {
-      user: "beulah.macgyver@ethereal.email", // generated ethereal user
-      pass: "C9sgaFmTKEA5a8xYpT", // generated ethereal password
+      user: process.env.SENDINBLUE_USER, // generated ethereal user
+      pass: process.env.SENDINBLUE_PASS, // generated ethereal password
     },
   });
 
   // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    from: '"Fred Foo 👻" <'+process.env.SENDINBLUE_USER+'>', // sender address
     to: "eoin@rockfieldmd.com, gaughraneoin@rockfieldmd.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
@@ -31,7 +29,7 @@ async function main() {
   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
   // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  /* console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info)); */
   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
